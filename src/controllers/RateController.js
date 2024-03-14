1a3636ca6e6a2bc4cbbe444f24eaaab617901f22
Kishan Raj <86339947+kishan20-00@users.noreply.github.com>
2024-03-15 00:43:33 +0530

    Added Discount Rate Codes

---
+++ src/controllers/RateController.js
@@ -0,0 +1,147 @@
+const rateDetails = require("../models/Rates.js");
+
+//add new Vehicle for system
+exports.addRate= async (req, res) => {
+ 
+    //constant variables for the attributes
+    const {
+        rateID,
+        discountHotelPercentage,
+        discountPackagePercentage,
+        discountSouveniourPercentage,
+        discountGiftsPercentage,
+        discountCollectiblePercentage,
+        discountVehiclePercentage,
+        discountAgentPercentage,
+        discountGuidePercentage,
+        hotelAdvertiseRate,
+        SouveniourAdvertiseRate,
+        giftAdvertiseRate,
+        collectibleAdvertiseRate,
+        vehicleAdvertiseRate,
+        agentRegisterRate,
+        guideRegisterRate
+     } = req.body;
+  
+  
+    rateDetails.findOne({rateID: rateID})
+      .then((savedRate) => {
+          if(savedRate) {
+              return res.status(422).json({error:"Vehicle already exists with that no"})
+          }
+  
+          const newRate = new rateDetails({
+            rateID,
+        discountHotelPercentage,
+        discountPackagePercentage,
+        discountSouveniourPercentage,
+        discountGiftsPercentage,
+        discountCollectiblePercentage,
+        discountVehiclePercentage,
+        discountAgentPercentage,
+        discountGuidePercentage,
+        hotelAdvertiseRate,
+        SouveniourAdvertiseRate,
+        giftAdvertiseRate,
+        collectibleAdvertiseRate,
+        vehicleAdvertiseRate,
+        agentRegisterRate,
+        guideRegisterRate
+           
+        })
+    
+        newRate.save().then(() => {
+             res.json("Rates Added")
+    
+        }).catch((err) => {
+          
+        })
+      
+    }).catch((err) =>{
+        
+    })
+    }
+
+//delete existing one
+exports.deleteRate = async (req, res) => {
+    let rateID = req.params.id;
+   
+    await rateDetails.findByIdAndDelete(rateID).then(() => {
+      res.status(200).json({ status: "Deleted Successfully" });
+    }).catch((error) => {
+      res.status(500).json({ status: "Error with Deleting", error: error.message });
+    })
+  }
+   
+ //update 
+ exports.updateRate= async (req, res) => { 
+    //fetch id from url
+    let id = req.params.id;
+    const {rateID,
+        discountHotelPercentage,
+        discountPackagePercentage,
+        discountSouveniourPercentage,
+        discountGiftsPercentage,
+        discountCollectiblePercentage,
+        discountVehiclePercentage,
+        discountAgentPercentage,
+        discountGuidePercentage,
+        hotelAdvertiseRate,
+        SouveniourAdvertiseRate,
+        giftAdvertiseRate,
+        collectibleAdvertiseRate,
+        vehicleAdvertiseRate,
+        agentRegisterRate,
+        guideRegisterRate
+           } = req.body;
+  
+    const updateRate = {
+        rateID,
+        discountHotelPercentage,
+        discountPackagePercentage,
+        discountSouveniourPercentage,
+        discountGiftsPercentage,
+        discountCollectiblePercentage,
+        discountVehiclePercentage,
+        discountAgentPercentage,
+        discountGuidePercentage,
+        hotelAdvertiseRate,
+        SouveniourAdvertiseRate,
+        giftAdvertiseRate,
+        collectibleAdvertiseRate,
+        vehicleAdvertiseRate,
+        agentRegisterRate,
+        guideRegisterRate }
+  
+  
+    const update = await rateDetails.findByIdAndUpdate(id, updateRate).then(() => {
+      res.status(200).send({status: "Result updated"})
+    }).catch((err) => {
+       
+        res.status(500).send({status: "Error with updating data", error: err.message});
+    })   
+  }
+
+//view 
+exports.viewRates= async (req, res) => { 
+ 
+    //calling  model
+    rateDetails.find().then((rates) => {
+      res.json(rates)
+  
+  }).catch((err) => {
+     
+  })
+  
+  }
+  //view one
+  exports.viewOneRate = async (req, res) => {
+    
+    let rateID = req.params.id;
+    const rate = await rateDetails.findById(rateID).then((rate) => {
+        res.status(200).send({status: "  fetched", rate})
+    }).catch(() => {
+        
+         res.status(500).send({status:"Error with get " , error: err.message})
+    })
+  }
