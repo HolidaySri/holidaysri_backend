63d0957e061c2d3c34ba6a0760c7c0d517402249
Yasara19 <86340688+Yasara19@users.noreply.github.com>
2024-07-26 07:05:26 +0530

    Edit and add images

--- src/models/Vehicle.js
+++ src/models/Vehicle.js
@@ -11,10 +11,18 @@ const VehicleSchema = new Schema(
       email: String,
       gender: String, 
       description: String,
-      images: String,
       location:String,
       promoCode: String,
-      driverStatus: String
+      driverStatus: String,
+      images: {
+        type: [String], // Array of strings to store image URLs
+        validate: {
+            validator: function (v) {
+                return v.length <= 6; // Validate that the array length is at most 6
+            },
+            message: props => `${props.value} exceeds the limit of 6 images per location!`
+        }
+    },
     },
   );
   
