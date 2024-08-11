94fbb83a94d84de7983b518585cac3ff02c77898
Kishan Raj <86339947+kishan20-00@users.noreply.github.com>
2024-08-11 22:55:02 +0530

    Added code for expired automation

--- src/models/promo_code.js
+++ src/models/promo_code.js
@@ -7,7 +7,7 @@ const promoCodeSchema = new mongoose.Schema({
   discountPercentage: { type: Number, required: true },
   expirationDate: { type: Date, required: true },
   isActive: { type: Boolean, default: true }, // Add isActive field
-});
+}, { timestamps: true });
 
 const PromoCode = mongoose.model('PromoCode', promoCodeSchema);
 
